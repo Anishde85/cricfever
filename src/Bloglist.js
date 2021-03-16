30c119dee5a718dfcbdf9d12f10af0df7ab0fd0e
Anish De <59112660+Anishde85@users.noreply.github.com>
2021-03-16 18:40:22 +0530

    Add files via upload

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,5 +1,6 @@
 import last from "./download.jpg" 
 import back from "./download (1).jpg"
+import back1 from "./download (2).jpg"
 export const list=[
     {
         id: 0,
@@ -14,5 +15,18 @@ export const list=[
         description: `After being demolished in less than two days England will be facing India in the fourth test starting on 4th march, 2021.After the 3rd match there were some questions about the pitch conditions,but India clarified the things quite well by giving the justification that 21 wickets out of 30 were on straighter deliveries. In an Interview Indian vice captain Ajinkya Rahane pointed out "Our technique gets critisized abroad, here visitors crib about pitch"-which answers all the questions of the critics.besides this pitch debate, another thing that's quite astonishing is the rotation policy and team selection of the visitors. They were the dominant side in the first test and after that they looked a different side altogether. But they have great talents in their team as well to come back strongly in the 4th test. Let's hope for a great contest. Cheers.`,
         link: "https://www.google.com/search?q=ind+vs+eng+4th+test&rlz=1C1CHBF_enIN868IN868&oq=ind&aqs=chrome.2.69i60j69i59l3j69i57j69i60j69i61l2.1796j1j7&sourceid=chrome&ie=UTF-8#sie=m;/g/11qpph5hv2;5;/m/021q23;dt;fp;1;;",
     },
+    {
+        id: 1,
+        likes:0,
+        data: back1,
+        heading: "The Battle of Equals at Ahmedabad.",
+        text: "",
+        data1: back1,
+        XI: `Predicted XI(India):Rohit Sharma, KL Rahul, Ishan Kishan, Virat Kohli(c), Rishabh Pant(w), Shreyas Iyer, Hardik Pandya, Washington Sundar, Shardul Thakur, Bhuvneshwar Kumar, Yuzvendra Chahal`,
+        XII:`Predicted XI(England) :Jason Roy,Johnny Bairstow,Dawid Malan,Eoin Morgan(C),Ben Stockes,Jos Buttler(w),Sam Curran,Adil Rashid,Jofra Archer,Mark wood,Chris Jordan.` ,
+        heading1: "The Battle of Equals at Ahmedabad.",
+        description: `After demolishing England in the Test series , the t20 series is now poised evenly at 1-1 . England dominated India in the first t20 whereas India thrashed England in the 2nd t20 . Its now time for the third one who’s gonna take the lead ? Will Ishan Kishan continue to show his fireworks alongside with hitman or will it be Jos Butler for England at the top? The third t20 is going to be played behind closed doors due to rising covid cases . So will the absence of 12 th man affect the blues ?  Bowling second hasn’t been upto the mark form both the teams  and dew had an heavy impact of the outcome of the game . So who’s gonna take a crucial lead in the series today?`,
+        link: "https://www.google.com/search?q=ind+vs+eng+t20&rlz=1C1CHBF_enIN868IN868&oq=ind+vs+eng&aqs=chrome.2.69i59j35i39j0i131i433j0i433l2j0i131i433l2j0i433j0i131i433l2.15747j0j7&sourceid=chrome&ie=UTF-8#sie=m;/g/11qppm9nll;5;/m/021q23;dt;fp;1;;",
+    }, 
 ]
 
